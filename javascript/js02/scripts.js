8373bc34cec88cc3332da6e90d0d267e4690fa60
gustavoags <94715362+gustavoags@users.noreply.github.com>
2022-02-02 12:26:16 -0300

    Atualizado

---
+++ javascript/js02/scripts.js
@@ -0,0 +1,7 @@
+// Comentário em linha.
+/*
+ Aqui se coloca comentários que ultrapassam o tamanho de uma linha, ou seja, duas ou mais linhas de códigos escritos
+ */
+
+console.log('Bem vindos ao Starter')
+console.log('Não iria ignorar')
