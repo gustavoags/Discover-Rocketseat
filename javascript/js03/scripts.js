098983aac90d50cb13caa9cd8f9bf62f71e34ff1
gustavoags <94715362+gustavoags@users.noreply.github.com>
2022-02-04 17:37:23 -0300

    Atualizado

---
+++ javascript/js03/scripts.js
@@ -0,0 +1,47 @@
+/*let weight;
+console.log(typeof weight)*/
+
+/*let name = "Gustavo"
+let age = 22
+let stars = 4.8
+let isSubscribed = true*/
+
+/*let student = {}
+console.log(student)*/
+
+/*let student = {
+    name: "Gustavo",
+    age: 23,
+    stars:4.8,
+    isSubscribed:true
+}
+console.log(student)*/
+
+let student = {
+    name:"Gustavo",
+    age:23,
+    weight:74.6,
+    stars:4.8,
+    isSubscribed: true
+}
+/*console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg`)*/
+
+/*let students =[]*/
+
+/*students = [
+    student
+]*/
+/*console.log(students)
+console.log(students[0])*/
+
+const john = {
+    name:"John",
+    age: 32,
+    weight: 78,
+    isSubscribed: true
+}
+students = [
+    student,
+    john
+]
+console.log(students[1])
