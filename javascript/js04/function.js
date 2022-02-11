80cda2a5351181b02ea894b61f27145d702105a0
gustavoags <94715362+gustavoags@users.noreply.github.com>
2022-02-11 10:49:57 -0300

    Atualizado

--- javascript/js04/function.js
+++ javascript/js04/function.js
@@ -1,61 +1,70 @@
-// Manipulando Strings e Números
+// Expressões e Operadores
 
-// String em número e número em string
-/*let string = "123"
-console.log(Number(string))
-let number = 321
-console.log(String(number))*/
+/* New
 
-// Contar quantos caracteres tem uma palavra e quantos digitos tem um número
-/*let word = "Paralelepípedo"
-console.log(word.length)
-let number = 1234
-console.log(String(number).length)*/
+    * left-hand-side expression
+    * criar um novo objeto
+*/
 
-// Transformar um número quebrado com 2 casas decimais e trocar ponto por vírgula
-/*let number = 435.21548558
-console.log(number.toFixed(2).replace(".", ","))*/
+/*let date = new Date('2022-02-11')
+console.log(date.__proto__)*/
 
-// Transforme letras minúsculas em maiúsculas. Faça o contrário disso também
-/*let word = "Programar é muito bacana!"
-console.log(word.toLowerCase().toUpperCase())*/
+//--------------
 
-//Separe um texto que contem espaços, em um novo array onde cada texto é uma posição do array. Depois disso, transforme o array em um texto e onde eram espaços, coloque _
-/*let phrase = "Eu quero muito aprender programação."
-let myArray = phrase.split(" ")
-let phraseWithUnderscore = myArray.join("_")
-console.log(phraseWithUnderscore.toLowerCase())*/
+// Typeof delete
+/*
+    Operadores unários
+    typeof
+    delete
+*/
+/*const person = {
+    name: 'Gustavo',
+    age: 23,
+}
+delete person.age  
 
-// Verificar se o texto contém a plavra Amor
-/*let phrase = "Eu quero viver o amor!"
-console.log(phrase.includes("amor"))*/
+console.log(person)*/
 
-// Manipulando Arrays
+//--------------------------
 
-// Criar Array com construtor
-/*let myArray = new Array('a', 'b', 'c')
-console.log(myArray)*/
+// Operadores Aritiméticos
 
-// Transformar uma cadeia de caracteres em elementos de um array
-/*let word = "manipulação"
-console.log(Array.from(word))*/
+// Multiplicação *
+//console.log(3.2 * 5)
 
-let techs = ["html", "css", "js"]
+// Divisão /
+//console.log(12/2)
 
-// adicionar um item no fim
-techs.push("nodejs")
-//adicionar no começo
-techs.unshift("sql")
-// remover do fim
-//techs.pop()
-// remover do começo
-//techs.shift()
-// pegar somente alguns elementos do array
-//console.log(techs.slice(1, 3))
-// remover 1 ou mais itens em qualquer posição do array
-//techs.splice(1, 2)
-// encontrar a posição de um elemento no array
-let index = techs.indexOf('html')
-techs.splice(index, 1)
+// Soma +
+//console.log(5+6)
 
-console.log(index)
+// Subtração -
+//console.log(8-2)
+
+// Resto de divisão %
+//let reminder
+//reminder = 11 % 3
+//console.log(reminder)
+
+// Incremento + +
+//let increment  = 0
+//increment++
+//console.log(increment)
+
+// Decremento --
+//let decrement = 5
+//decrement--
+//console.log(decrement)
+
+// Exponencial **
+//console.log(3 ** 2)
+
+//-----------------------
+
+// Grouping operator ( )
+
+//let total = 2 + 5 * 5
+//console.log(total)
+
+//let total = (2 + 5) * 5
+//console.log(total)
