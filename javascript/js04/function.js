9bf086005d21c6b898570af579b4f0072e9e7c35
gustavoags <94715362+gustavoags@users.noreply.github.com>
2022-02-15 11:26:37 -0300

    Atualizado

--- javascript/js04/function.js
+++ javascript/js04/function.js
@@ -1,104 +1,26 @@
-// Condicionais e controle de fluxo
+// Estruturas de repetição
 
-// Controle de fluxo da aplicação
-// If and Else
+// For (inicialização de uma variável; condição de cotinuação para o loop; expressão final)
+// break -> para a execução do loop
+// continue -> pula a execução do momento
 
-/*let temperature = 36.5
-
-if( temperature >= 37) { 
-    console.log('Febre')}
-else {
-    console.log('Saudável')
-}
-*/
-
-/*let temperature = 36.9
-let hightTemperature = temperature >= 37.5
-let mediumTemperature = temperature >= 37.0 && temperature < 35.7
-
-if(hightTemperature) {
-    console.log('Febre alta')
-}
-else if(mediumTemperature) {
-    console.log('Febre moderada')
-}
-else {
-    console.log('Saudável')
-}
-*/
-
-//---------------------------------------------------------------------------------
-
-// Switch
-
-/*let expression = 'a'
-
-switch (expression) {
-    case 'a':
-        // código
-        console.log('a')
-        break
-    case 'b':
-        // código para expression b
-        console.log('b')
-        break
-    default:
-        console.log('default')
-        break
-}
-*/
-
-/*function calculate(number1, operator, number2) {
-    let result = 0;
-
-    switch (operator) {
-        case '+':
-            result = number1 + number2
-            break
-        case '-':
-            result = number1 - number2
-            break
-        case '*':
-            result = number1 * number2
-            break
-        case '/':
-            result = number1 /  number2
-            break 
-        case '%':
-            result = number1 % number2
-            break     
-        default:
-            console.log('Não implementado')            
+/*for(let i = 10; i > 0; i--) {
+    if(i === 5) {
+        continue;
     }
 
-    return result
-}
-
-console.log(calculate(4, '%', 8))
-*/
-
-//--------------------------------------------------------------------------------
-
-// Throw 
-
-/*function sayMyName(name = '') {
-    if (name === '') {
-        throw "Nome é necessário"
-    }
-
-    console.log(name)
+    console.log(i)
 }
 */
 
-// Try/Catch
+// While
 
-/*try {
-    sayMyName('Gustavo')
+/*let i = 500;
+while(i > 10) {
+    console.log(i)
 
-} catch(e) {
-    console.log(e)
-
+    i /= 5;
 }
-
-console.log('Após o Try/Catch')
 */
+
+     
