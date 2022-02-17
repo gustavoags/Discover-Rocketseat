f191d0b66cb3788083c2e27380565ae48d232d93
gustavoags <94715362+gustavoags@users.noreply.github.com>
2022-02-17 10:50:37 -0300

    Atualizado

--- javascript/js04/function.js
+++ javascript/js04/function.js
@@ -32,6 +32,8 @@ return scoreFinal
 console.log(getScore(60))
 */
 
+//--------------------------------------------------------------------------------------------
+
 // Fluxo de caixa familiar
 /*
 let familiarExpenses = {
@@ -67,4 +69,39 @@ function calculateBalance() {
 }
 
 calculateBalance()
+*/
+
+//-----------------------------------------------------------------------------------------------
+
+// Celsius to Fahrenheit
+/*
+function transformDegree(degree) {
+    const celsiusExists = degree.toUpperCase().includes('C')
+    const fahrenheitExists = degree.toUpperCase().includes('F')
+
+    if(!celsiusExists && !fahrenheitExists) {
+        throw new Error("Grau não identificado")
+    }
+
+    // Fluxo ideal, F para C
+
+    let updateDegree = Number(degree.toUpperCase().replace("F", ""));
+    let formula = (fahrenheit) => (fahrenheit - 32) * 5/9
+    let degreeSign = 'C'
+
+    if(celsiusExists) {
+        updateDegree = Number(degree.toUpperCase().replace("C", ""));
+        formula = (celsius) => (celsius * 9/5 + 32)
+        degreeSign = 'F'
+    }  
+
+    return formula(updateDegree) + degreeSign
+}
+
+try {
+    console.log(transformDegree('100C'))
+    //transformDegree('50Z')
+} catch(error) {
+    console.log(error.message)
+}
 */
