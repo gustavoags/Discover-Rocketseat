d60689e6ee0bf1d8e727a42234e00a8f0488beca
gustavoags <94715362+gustavoags@users.noreply.github.com>
2022-02-18 11:34:05 -0300

    Atualizado

--- javascript/js04/function.js
+++ javascript/js04/function.js
@@ -105,3 +105,87 @@ try {
     console.log(error.message)
 }
 */
+
+//----------------------------------------------------------------------------------
+
+
+// Buscando e encontrando dados de um Array
+
+/*
+const booksByCategory = [
+    { 
+        category: "Riqueza",
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author: "T. Harv Eker",
+            },
+            {
+                title: "O homem mais rico da Babilônia",
+                author: "George S. Classon",
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+
+        ],
+    },
+    { 
+        category: "Inteligência Emocional",
+        books: [
+            {
+                title: "Você é insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title : "Ansiedade - Como enfrentar o mal do século",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes",
+                author: "Stephen R. Covey",
+            },
+
+        ],
+    },
+];
+
+const totalCategory = booksByCategory.length
+
+//console.log(totalCategories)
+for(let category of booksByCategory) {
+    console.log('Total de livors da categoria: ', category.category)
+    console.log(category.books.length)   
+}
+
+function countAuthors() {
+    let authors = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(authors.indexOf(book.author) == -1) {
+                authors.push(book.author)
+            }
+        }
+    }
+    console.log("Total de autores: ", authors.length)
+}
+
+countAuthors();
+
+function booksOfAuthor(author) {
+    let books = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books){
+            if(book.author === author) {
+                books.push(book.title)
+            }
+        }
+    }
+    console.log(`Livros do autor ${author}: ${books.join(', ')}`)
+}
+
+booksOfAuthor('George S. Classon')
+*/
